0e5a791224c6df62466c63974d5a92e0b7241208
omunoz <steven.120189@gmail.com>
2021-05-18 22:21:52 -0400

    Added post route, still need to send data back to imgflip.

--- controllers/bank.js
+++ controllers/bank.js
@@ -6,9 +6,6 @@ const db = require('../models');
 const isLoggedIn = require('../middleware/isLoggedIn');
 
 
-// router.get('/', (req, res) => {
-//     res.render('bank');
-//   });
 
   router.get("/", isLoggedIn, async function (req, res) {
     const findMemes = await db.meme.findAll({
@@ -16,12 +13,7 @@ const isLoggedIn = require('../middleware/isLoggedIn');
     res.render("bank", {memes: findMemes });
   });
 
-  // router.get('/', isLoggedIn, (req, res) => {
-  //   // const { id } = req.user.get();
-  //   const findMemes = db.meme.findAll({
-  //     where:{userId: 1}});
-  //   res.render('bank', { memes: findMemes});
-  // });
+  
 
 
 module.exports = router;
--- controllers/meme.js
+++ controllers/meme.js
@@ -3,17 +3,31 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const db = require('../models');
+const fetch = require('node-fetch');
 
-router.get('/', (req,res) => {
-  axios.get('https://api.imgflip.com/get_memes')
-  .then(response => {
-    let memes = response.data.data.memes
-    // console.log(memes)
-    res.render('meme', {meme:memes})
-  })
+// router.get('/', (req,res) => {
+//   axios.get('https://api.imgflip.com/get_memes')
+//   .then(response => {
+//     let memes = response.data.data.memes
+//     // console.log(memes)
+//     res.render('meme', {meme:memes})
+//   })
+// })
+
+router.get('/', async (req,res) => {
+  const response = await fetch('https://api.imgflip.com/get_memes');
+  const responseData = await response.json();
+  const memes = await responseData.data.memes
+  const meme0 = await memes[0]
+    // console.log(memeImg)
+    res.render('meme', {meme:memes, meme0:meme0})
+
 })
 
-
+router.post('/', async (req,res) => {
+  const {template,top_text,bottom_text} = req.body;
+  console.log(req.body)
+})
 
 
 
