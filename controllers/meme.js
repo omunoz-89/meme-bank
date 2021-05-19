23ee96b67f8237d604227f987bbee03d7b6a3b09
omunoz <steven.120189@gmail.com>
2021-05-19 12:49:43 -0400

    meme.js Controller: Post route response, getting back generated meme

--- controllers/meme.js
+++ controllers/meme.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const axios = require('axios');
 const db = require('../models');
 const fetch = require('node-fetch');
+require('dotenv').config()
 
 
 router.get('/', async (req,res) => {
@@ -11,7 +12,6 @@ router.get('/', async (req,res) => {
   const responseData = await response.json();
   const memes = await responseData.data.memes
   const meme0 = await memes[0]
-    // console.log(memeImg)
     res.render('meme', {meme:memes, meme0:meme0})
 
 })
@@ -19,6 +19,14 @@ router.get('/', async (req,res) => {
 router.post('/', async (req,res) => {
   const {memeId,top_text,bottom_text} = req.body;
   console.log(req.body)
+  try {
+    const response = await fetch(`https://api.imgflip.com/caption_image?template_id=${memeId}&username=${process.env.IMG_USER}&password=${process.env.IMG_PASS}&text0=${top_text}&text1=${bottom_text}`)
+    const responseData = await response.json()
+    const img = responseData.data.url
+  } catch (error) {
+    console.log('------ Error ------');
+    console.log(error);
+  }
 })
 
 
